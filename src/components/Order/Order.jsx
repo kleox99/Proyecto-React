dd670f83e466eddd8440b033f61b32611260df04
kleox99 <sebastiantito1@gmail.com>
2023-04-06 00:03:51 -0300

    Retoque final

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { OrderContainer } from './OrderStyled';
 
 const Order = () => {
@@ -7,22 +7,26 @@ const Order = () => {
     const location = useLocation();
     const {order} = location.state;
     
-    return (
-        <OrderContainer>
-            <h1>¡Gracias {order.user.nombre} por su preferencia!</h1>
-            <span>
-                Su codigo de compra es: <b>"{order.id}"</b>
-            </span>
-            <span>
-                Su recibo ha sido enviado a su casilla {order.user.email}
-            </span>
-            {!order.user.pagoOnline && 
-            <span>
-                Recuerde que tendrá que abonar el pedido al momento de la entrega.
-            </span>
-            }
+    const navigate = useNavigate();
 
-        </OrderContainer>
+    return (
+        <>
+            <OrderContainer>
+                <h1>¡Gracias {order.user.nombre} por su preferencia!</h1>
+                <span>
+                    Su codigo de compra es: <b>"{order.id}"</b>
+                </span>
+                <span>
+                    Su recibo ha sido enviado a su casilla {order.user.email}
+                </span>
+                {!order.user.pagoOnline && 
+                <span>
+                    Recuerde que tendrá que abonar el pedido al momento de la entrega.
+                </span>
+                }
+            </OrderContainer>
+            <button onClick={() => navigate("/")}>Volver al incio</button>
+        </>
     )
 }
 
